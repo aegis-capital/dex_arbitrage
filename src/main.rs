a78477827f13d65e982dc4122b245238fbe0db28
mgmarltd <gencehanarpacay@gmail.com>
2024-02-09 02:12:12 +0300

    check update

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> web3::Result {
                         }
                         info!("From: {:?}", txn.from );
                         info!("Value: {:?}", txn.value);
-                        if txn.to = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"{
+                        if txn.to == "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"{
                             println!("To: {:?}", txn.to);
                             println!("From: {:?}", txn.from );
                             println!("Value: {:?}", txn.value);
