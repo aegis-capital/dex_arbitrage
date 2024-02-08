323e2e0f01d945147e02b7da28dac24e704dd706
mgmarltd <gencehanarpacay@gmail.com>
2024-02-09 02:20:11 +0300

    all

--- src/main.rs
+++ src/main.rs
@@ -32,8 +32,8 @@ async fn main() -> web3::Result {
                         if let Some(to) = txn.to {
                             if to == "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D"{
                                 println!("To: {:?}", to);
-                                println!("From: ", txn.from);
-                                println!("value: ", txn.value);
+                                println!("From: {:?}", txn.from);
+                                println!("value: {:?}", txn.value);
                             }
                         } else {
                             warn!("Transaction does not have a 'to' address");
